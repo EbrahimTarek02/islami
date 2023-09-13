8f64a362416c4200b2769ca71a9c3d2048f08003
EbrahimTarek02 <108439640+EbrahimTarek02@users.noreply.github.com>
2023-09-13 22:32:08 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:islami/ui/quran_screen/quran_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      routes: {
+        QuranScreen.routeName : (_) => QuranScreen(),
+      },
+
+      initialRoute: QuranScreen.routeName,
+    );
+  }
+}
---
+++ lib/ui/quran_screen/quran_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class QuranScreen extends StatelessWidget {
+
+  static const String routeName = 'Quran Screen Route Name';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
